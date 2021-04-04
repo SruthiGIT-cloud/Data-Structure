b37532e453948a8c4cadb80a9b74df9c178b0ea3
Sruthi A R <78865129+SruthiGIT-cloud@users.noreply.github.com>
2021-04-04 14:56:44 +0530

    Create reverse k elements.c
    Reversing the first K elements of a Queue

---
+++ Queue/reverse k elements.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int r=-1,f=-1;
+int q[100];
+void enqueue(int data,int n)
+{
+    if(r==-1||f==-1)
+    {
+        r=f=0;
+        q[r]=data;
+    }
+    else
+    {
+        r++;
+        q[r]=data;
+    }
+}
+void reverse(int k)
+{
+  k=k-1;
+    for(int i=f,j=k;i<j;j--,i++)
+    
+    {
+        int temp=q[j];
+        q[j]=q[i];
+        q[i]=temp;
+    }
+    
+}
+void printz()
+{
+    for(int i=f;i<=r;i++)
+    {
+        printf("%d ",q[i]);
+    }
+}
+void main()
+{
+    int n,data,k;
+    printf("Enter size\n");
+    scanf("%d",&n);
+    printf("Enter data\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&data);
+        enqueue(data,n);
+    }
+    printf("Enter k value\n");
+    scanf("%d",&k);
+    reverse(k);
+    printz();
+}
+
